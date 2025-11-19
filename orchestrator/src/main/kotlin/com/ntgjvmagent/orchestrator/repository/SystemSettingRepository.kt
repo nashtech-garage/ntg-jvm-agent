77afec975a63cc5655403c2fac176fa551a1ac62
Nhien Tran Duc <nhientrand@nashtechglobal.com>
2025-11-19 14:30:36 +0700

    Fix CI

--- orchestrator/src/main/kotlin/com/ntgjvmagent/orchestrator/repository/SystemSettingRepository.kt
+++ orchestrator/src/main/kotlin/com/ntgjvmagent/orchestrator/repository/SystemSettingRepository.kt
@@ -4,6 +4,4 @@ import com.ntgjvmagent.orchestrator.entity.SystemSettingEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface SystemSettingRepository : JpaRepository<SystemSettingEntity, UUID> {
-
-}
+interface SystemSettingRepository : JpaRepository<SystemSettingEntity, UUID>
---
+++ orchestrator/src/test/kotlin/com/ntgjvmagent/orchestrator/integration/systemsetting/SystemSettingControllerIT.kt
@@ -0,0 +1,44 @@
+package com.ntgjvmagent.orchestrator.integration.systemsetting
+
+
+import com.ntgjvmagent.orchestrator.entity.SystemSettingEntity
+import com.ntgjvmagent.orchestrator.integration.BaseIntegrationTest
+import com.ntgjvmagent.orchestrator.repository.SystemSettingRepository
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+
+@DisplayName("SystemSettingController Integration Tests")
+class SystemSettingControllerIT
+    @Autowired
+    constructor(
+        private val repository: SystemSettingRepository,
+
+        ) : BaseIntegrationTest() {
+    @Test
+    fun `should get SystemSetting`() {
+        repository.save(
+            SystemSettingEntity(
+                siteName = "TEST",
+                    maximumUser = 10,
+                    sessionTimeout = 10,
+                    maximumSizeFileUpload = 10,
+                    allowedFileTypes = "doc, pdf",
+                    maintenanceMode = true,
+                    userRegistration = true,
+                    emailVerification = true,
+            ),
+        )
+
+        mockMvc
+            .perform(
+                getAuth("/api/system", roles = listOf("ROLE_ADMIN")),
+            ).andExpect(status().isOk)
+            .andExpect(jsonPath("$[0].siteName").value("TEST"))
+            .andExpect(jsonPath("$[0].maximumUser").value(10))
+    }
+
+}
