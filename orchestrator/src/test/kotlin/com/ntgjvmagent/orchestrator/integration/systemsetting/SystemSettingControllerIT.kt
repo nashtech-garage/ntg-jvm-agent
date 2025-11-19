f7eb402ab8bf833a0375cb28882bc942b11ef132
Nhien Tran Duc <nhientrand@nashtechglobal.com>
2025-11-19 15:27:51 +0700

    Fix CI: update format

--- orchestrator/src/test/kotlin/com/ntgjvmagent/orchestrator/integration/systemsetting/SystemSettingControllerIT.kt
+++ orchestrator/src/test/kotlin/com/ntgjvmagent/orchestrator/integration/systemsetting/SystemSettingControllerIT.kt
@@ -1,6 +1,5 @@
 package com.ntgjvmagent.orchestrator.integration.systemsetting
 
-import com.ntgjvmagent.orchestrator.entity.SystemSettingEntity
 import com.ntgjvmagent.orchestrator.integration.BaseIntegrationTest
 import com.ntgjvmagent.orchestrator.repository.SystemSettingRepository
 import org.junit.jupiter.api.DisplayName
@@ -10,30 +9,16 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @DisplayName("SystemSettingController Integration Tests")
 class SystemSettingControllerIT
-@Autowired
-constructor(
-    private val repository: SystemSettingRepository,
-) : BaseIntegrationTest() {
-    @Test
+    @Autowired
+    constructor(
+        private val repository: SystemSettingRepository,
+    ) : BaseIntegrationTest() {
+        @Test
     fun `should get SystemSetting`() {
-        repository.save(
-            SystemSettingEntity(
-                siteName = "TEST",
-                maximumUser = 10,
-                sessionTimeout = 10,
-                maximumSizeFileUpload = 10,
-                allowedFileTypes = "doc, pdf",
-                maintenanceMode = true,
-                userRegistration = true,
-                emailVerification = true,
-            ),
-        )
-
         mockMvc
             .perform(
                 getAuth("/api/system", roles = listOf("ROLE_ADMIN")),
             ).andExpect(status().isOk)
             .andReturn()
-
+        }
     }
-}
