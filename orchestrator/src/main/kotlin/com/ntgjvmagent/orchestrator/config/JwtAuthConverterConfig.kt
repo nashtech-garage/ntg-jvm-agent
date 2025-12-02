ceea54994a8b8d05ce1c579dab50f33f055ef387
LongLeVanQuoc <long.levanquoc@nashtechglobal.com>
2025-12-02 13:56:57 +0700

    Chat-ui: share conversation

--- orchestrator/src/main/kotlin/com/ntgjvmagent/orchestrator/config/JwtAuthConverterConfig.kt
+++ orchestrator/src/main/kotlin/com/ntgjvmagent/orchestrator/config/JwtAuthConverterConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
 import org.springframework.core.env.Environment
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.GrantedAuthority
