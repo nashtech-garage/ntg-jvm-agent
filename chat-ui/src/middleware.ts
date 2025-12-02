37fee8c1b4b711eea94f643272068395b8c4ff9a
kientran <>
2025-12-02 16:35:49 +0700

    chore(chat-ui): restructure files and centralize env/log

--- chat-ui/src/middleware.ts
+++ chat-ui/src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { setTokenIntoCookie, getRefreshToken, deleteCookies } from './utils/server-utils';
-import { authPathname } from '@/constants/url';
 
 export async function middleware(req: NextRequest) {
   const pathName = req.nextUrl.pathname;
