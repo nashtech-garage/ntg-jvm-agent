36f1475ef121bddab96d6e69378198cb0ff76b6c
kientran <>
2025-12-04 08:31:28 +0700

    refactor: switch to use NextAuth (update env docker)

--- authorization-server/src/main/kotlin/com/ntgjvmagent/authorizationserver/config/RegisteredClientConfig.kt
+++ authorization-server/src/main/kotlin/com/ntgjvmagent/authorizationserver/config/RegisteredClientConfig.kt
@@ -37,7 +37,7 @@ class RegisteredClientConfig {
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .redirectUri("http://localhost:3000/auth/callback") // Chat UI
-                .redirectUri("http://localhost:3001/auth/callback") // Admin UI
+                .redirectUri("http://localhost:3001/api/auth/callback/auth-server") // Admin UI
                 .scope(OidcScopes.OPENID)
                 .scope(OidcScopes.PROFILE)
                 .scope("chatbot.read")
