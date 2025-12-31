5407ad7319340e6d53b0abfcb488691b712d912c
BaoLeHuy <ahreynohope@gmail.com>
2025-12-31 17:58:45 +0700

    chore: fix arcording to copilot reviews

--- orchestrator/src/main/kotlin/com/ntgjvmagent/orchestrator/advisor/ChatMemoryAdvisor.kt
+++ orchestrator/src/main/kotlin/com/ntgjvmagent/orchestrator/advisor/ChatMemoryAdvisor.kt
@@ -1,5 +1,6 @@
 package com.ntgjvmagent.orchestrator.advisor
 
+import com.ntgjvmagent.orchestrator.config.CustomChatMemoryConfig
 import com.ntgjvmagent.orchestrator.repository.ChatMessageRepository
 import com.ntgjvmagent.orchestrator.utils.Constant
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor
@@ -9,11 +10,11 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class ChatMemoryAdvisor {
     @Bean
-    fun customChatMemory(chatMessageRepository: ChatMessageRepository): CustomChatMemory =
-        CustomChatMemory(chatMessageRepository)
+    fun customChatMemory(chatMessageRepository: ChatMessageRepository): CustomChatMemoryConfig =
+        CustomChatMemoryConfig(chatMessageRepository)
 
     @Bean
-    fun messageChatMemoryAdvisor(chatMemory: CustomChatMemory): MessageChatMemoryAdvisor =
+    fun messageChatMemoryAdvisor(chatMemory: CustomChatMemoryConfig): MessageChatMemoryAdvisor =
         MessageChatMemoryAdvisor
             .builder(chatMemory)
             .order(Constant.MESSAGE_HISTORY_ADVISOR_ORDER)
